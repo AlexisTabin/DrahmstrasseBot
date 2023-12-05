9b25d28359cd5d2009704df7b22db1b2ab3c0f86
AT's PI <alexis.tabin@gmail.com>
2023-12-05 10:00:29 +0100

    Cannot activate conda env from script on the raspberry :(

--- run-on-start-pi.sh
+++ run-on-start-pi.sh
@@ -1,9 +1,10 @@
-#!/bin/sh
+#!/bin/bash
+
 if [ -f "/home/pi/miniconda3/etc/profile.d/conda.sh" ]; then
     . "/home/pi/miniconda3/etc/profile.d/conda.sh"
     CONDA_CHANGEPS1=false conda activate py36
 fi
-conda activate py36
+. /home/pi/miniconda3/bin/activate py36
 cd  ~/Documents/DrahmstrasseBot/
 git pull
 python3 drahmbot.py
